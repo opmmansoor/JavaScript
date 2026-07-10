cf238d73afcd4ab908901e6f69d4fadc6f155b14
opmmansoor <opmmansoor@gmail.com>
2026-07-10 11:24:38 +0530

    Java Script - INTRO

---
+++ sample1.js
@@ -0,0 +1,48 @@
+//Java Script 
+/*JavaScript (JS) is a programming language used to make websites interactive and dynamic.
+JavaScript is a high-level programming language used to make websites interactive and dynamic. 
+It allows us to respond to user actions, update webpage content, perform calculations, 
+validate forms, and communicate with servers. JavaScript can run in web browsers and on servers 
+using Node.js.
+
+JavaScript is a high-level, interpreted programming language used to create interactive and 
+dynamic websites and web applications. It works together with HTML and CSS to control the 
+behavior of web pages, respond to user actions, manipulate content, communicate with servers, 
+and build both frontend and backend applications. It is one of the most widely used programming 
+languages in modern web development.
+
+** Intractive : Website Responds to the user's action
+** Dynamic    : Website content changes automatically
+** Interpreted: code exicution line by line.
+
+JavaScript is a programming language that makes websites interactive. It works with HTML and CSS 
+to build modern web applications. */
+
+
+// JavaScript is one of the three core technologies of web development:
+
+// HTML → Creates the structure (the skeleton)
+// CSS → Adds style (the clothes)
+// JavaScript → Adds behavior (the brain) 
+
+
+
+
+/* Without JavaScript, a website is mostly static. With JavaScript, you can:
+Show or hide content
+Validate forms
+Create sliders and animations
+Build calculators
+Display pop-up messages
+Fetch data from a server without reloading the page
+Build complete web applications
+
+Advantages
+Easy to learn
+Huge community
+Works in all modern browsers
+Large number of libraries and frameworks
+Can be used for frontend and backend development
+*/
+
+
