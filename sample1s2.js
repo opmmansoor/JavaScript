2f0858f8db165a2ba5de605a414fa543c8942dbd
opmmansoor <opmmansoor@gmail.com>
2026-07-10 14:23:32 +0530

    Link,Display & Comment

---
+++ sample1s2.js
@@ -0,0 +1,55 @@
+//link 
+//javascript folder link / connect to the HTML File
+//Write inside of body  <script src="foldername.js"> </script>
+
+
+
+
+//JavaScript DISPLAY
+
+/*JavaScript Display means showing output (data or messages) to the user or developer.
+When your JavaScript code runs, you often want to see the result. JavaScript provides different ways 
+to display output depending on where you want it to appear.
+
+The five most common display methods are:
+*****************************************
+1.console.log() → Shows output in the browser's Console.
+2.document.write() → Writes directly to the HTML page.
+3.alert() → Shows a pop-up message.
+4.prompt() → is a JavaScript method that displays a popup dialog with a text input box.
+5.innerHTML → Displays or changes content inside an HTML element. */
+
+
+//This is my first Javascript programme
+
+//document.write()
+document.write("Learn Javascript")
+
+//commen alert
+alert("Hello World") 
+
+//console.log()
+console.log("hai");
+console.log(10+15);
+
+//innerHTML
+document.getElementById("head").innerHTML = "hello";
+//( changed the html page h1 tag (hai)to (hello))
+
+//prompt()
+prompt("Enter your Name");
+
+
+
+
+//COMMENT
+// A comment is text that JavaScript ignores.
+//Comments are written to explain the code, make it easier to understand, or temporarily stop some code 
+//from running.
+
+// 1. Single-line Comment (//)
+
+// 2. Multi-line Comment (/* */)
+/*
+this is comment
+*/
